6bc66de885f4756a2a4b9619577d9980b70f902d
Serhii Artyshchuk <serban9999@gmail.com>
2022-09-13 16:16:56 +0300

    added a task

---
+++ js/task-01.js
@@ -0,0 +1,6 @@
+const categories = document.querySelector('#categories');
+console.log(categories.children);
+const category = document.querySelector('h2')
+console.log(category.firstChild);
+const elements = document.querySelectorAll('.item li')
+console.log(elements);
