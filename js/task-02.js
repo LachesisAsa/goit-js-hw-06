89eb66a7220ef27cd900224c3d5fbe016a50ed4f
Serhii Artyshchuk <serban9999@gmail.com>
2022-09-13 17:03:55 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,11 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const products = ingredients.map((ingredient) => {
+  const item = document.createElement("li");
+  item.textContent = ingredient;
+  item.className = "item";
+  return item;
+});
+
+document.querySelector("ul#ingredients").append(...products);
