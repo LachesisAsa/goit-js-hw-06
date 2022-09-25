d2c7e215a34c072e251c3b196f24a2d53ddeb77a
Serhii Artyshchuk <serban9999@gmail.com>
2022-09-25 17:12:00 +0300

    task accomplished

--- js/task-03.js
+++ js/task-03.js
@@ -11,4 +11,15 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
-];
+]
+const imageEl = [...images].map((imageEl) => 
+ `<li><img 
+ src="${imageEl.url}" 
+ alt="${imageEl.alt}" 
+ height="210px"></li>`
+);
+const gallery = document.querySelector(".gallery");
+gallery.style.display = "flex";
+gallery.style.listStyleType = "none";
+gallery.style.columnGap = "10px";
+gallery.insertAdjacentHTML("afterbegin", imageEl);
